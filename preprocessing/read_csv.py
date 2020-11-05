88702505133bd146b2a0a845a87fb901bde29702
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-11-04 21:25:50 -0600

    Renaming class ReadCSV to PreProcessCSV, removing pandas.read_csv - now we will assume that the data was already read with pd.read_csv and was passed to PreProcessCSV

--- preprocessing/read_csv.py
+++ preprocessing/read_csv.py
@@ -1,43 +1,47 @@
-import pandas as pd
 
-class ReadCSV:
-    def __init__(self, filepath):
-        """
 
-        :param filepath:
+class PreProcessCSV:
+
+    def __init__(self, data):
         """
-        self.filepath = filepath
-        self.data = pd.read_csv(filepath_or_buffer=filepath)
+        import pandas as pd
+
+        data = pd.read_csv("some_csv_dataset.csv")
+        PreProcessCSV(data)
+
+        :param data: Incoming Pandas DataFrame
+        """
+        self.data = data
 
     def get_size(self):
         """
-        Return the number of rows and columns within Pandas DataFrame
-        :return:
+        Returns the number of rows and columns within Pandas DataFrame
+        :return: A tuple (rows, columns)
         """
         return self.data.shape
 
     def remove_col_by_name(self, column_name):
         """
-        Remove a column from Pandas DataFrame by name
+        Removes a column from Pandas DataFrame by name
         :param column_name: The name of the column we would like to remove
-        :return:
+        :return: None
         """
         assert type(column_name) == str
         del self.data[column_name]
 
     def add_col_by_name(self, column_name, input_data):
         """
-        Add a new column with new data
+        Adds a new column with new data
         :param column_name: The name of the column we would like to add
         :param input_data: The input data array
-        :return:
+        :return: None
         """
         assert len(input_data) == len(self.data.index)
         self.data[column_name] = input_data
 
     def to_list(self):
         """
-        Convert Pandas DataFrame into a list of lists
-        :return:
+        Converts a Pandas DataFrame into a list of lists
+        :return: A list of lists
         """
         return self.data.values.tolist()
