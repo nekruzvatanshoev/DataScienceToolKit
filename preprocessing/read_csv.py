2d7bd2f13da2cdf76f9d7e1982a5cf3957cd30bc
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-11-04 19:48:03 -0600

    Added some documentation and a to_list method

--- preprocessing/read_csv.py
+++ preprocessing/read_csv.py
@@ -2,16 +2,24 @@ import pandas as pd
 
 class ReadCSV:
     def __init__(self, filepath):
+        """
+
+        :param filepath:
+        """
         self.filepath = filepath
         self.data = pd.read_csv(filepath_or_buffer=filepath)
 
     def get_size(self):
+        """
+        Return the number of rows and columns within Pandas DataFrame
+        :return:
+        """
         return self.data.shape
 
     def remove_col_by_name(self, column_name):
         """
-
-        :param column_name: The column name we would like to remove
+        Remove a column from Pandas DataFrame by name
+        :param column_name: The name of the column we would like to remove
         :return:
         """
         assert type(column_name) == str
@@ -19,10 +27,17 @@ class ReadCSV:
 
     def add_col_by_name(self, column_name, input_data):
         """
-
-        :param column_name: The column name for our new column
+        Add a new column with new data
+        :param column_name: The name of the column we would like to add
         :param input_data: The input data array
         :return:
         """
         assert len(input_data) == len(self.data.index)
         self.data[column_name] = input_data
+
+    def to_list(self):
+        """
+        Convert Pandas DataFrame into a list of lists
+        :return:
+        """
+        return self.data.values.tolist()
